52b9aecdf6189239276e7fafe80b11cf0bd48b0b
Diksha Sharma <46916373+diksha09@users.noreply.github.com>
2019-10-18 12:28:10 +0530

    Update settings.py

--- zenoo/settings.py
+++ zenoo/settings.py
@@ -127,5 +127,5 @@ STATIC_URL = '/static/'
 EMAIL_USE_TLS = True  
 EMAIL_HOST = 'smtp.gmail.com'  
 EMAIL_PORT = 587  
-EMAIL_HOST_USER = 'diksha455sharma@gmail.com'  
-EMAIL_HOST_PASSWORD = 'diksha@123'
+EMAIL_HOST_USER = ''  
+EMAIL_HOST_PASSWORD = ''
